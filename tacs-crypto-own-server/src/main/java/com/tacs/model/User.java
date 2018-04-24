6833f41ca01e0ec42961066f7f054db4976f3803
Sergio Cano <serdeliverance@gmail.com>
2018-04-24 14:48:27 -0300

    Client project created and new folder structure arranged

---
+++ tacs-crypto-own-server/src/main/java/com/tacs/model/User.java
@@ -0,0 +1,35 @@
+package com.tacs.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Random;
+
+@Setter
+@Getter
+public class User {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String nick;
+    private Byte age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", nick='" + nick + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public User(String name, String surname, Byte age, String nick) {
+        this.id = new Random().nextInt();
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.nick = nick;
+    }
+
+}
